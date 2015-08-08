177ed7fde873b3220768f3d4bfae07ece680a268
Freeman1989 <xianglijiang1989@gmail.com>
2015-08-08 16:09:09 +0800

    C++继承学习

---
+++ succession/Person.h
@@ -0,0 +1,13 @@
+#include <string>
+using namespace std;
+
+class Person
+{
+public:
+    Person();
+    ~Person();
+    void eat();
+    string m_strName;
+    int m_iAge;
+};
+
---
+++ succession/Worker.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Worker.h"
+using namespace std;
+
+Worker::Worker()
+{
+    cout << "Worker()" << endl;
+}
+
+Worker::~Worker()
+{
+    cout << "~Worker()" << endl;
+}
+
+void Worker::work()
+{
+    cout << "work()" << endl;
+}
+
